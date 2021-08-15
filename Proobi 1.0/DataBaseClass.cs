a182cb2b3ceae9eaeac057a6bdb14673cb854abf
Proobi <ramzi.benameur@gmail.com>
2021-08-15 20:19:05 +0100

    finishing add and update and delete function for each table in the database class

--- Proobi 1.0/DataBaseClass.cs
+++ Proobi 1.0/DataBaseClass.cs
@@ -80,5 +80,187 @@ namespace Proobi_1._0
 
             }
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <param name="Password"></param>
+        /// <param name="Name"></param>
+        /// <param name="Email"></param>
+        /// <param name="Adress"></param>
+        /// 
+        //Adding User
+        public void AddUser(string UserName, string Password, string Name, string Email, string Adress)
+        {
+            string query = "INSERT INTO User (UserName, Password, Name, Email, Adress) VALUES ('" +
+                UserName + "','" +
+                Password + "','" +
+                Name + "','" +
+                Email + "','" +
+                Adress + "')";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+
+        }
+
+        //Updating  User
+        public void UpdateUser(int IDUser, string UserName, string Password, string Name, string Email, string Adress)
+        {
+            string query = "UPDATE User SET UserName ='" + UserName
+                + "',  Password='" + Password
+                + "',  Name='" + Name
+                + "',  Email='" + Email
+                + "',  Adress='" + Adress
+                + "' WHERE IDUser='" + IDUser.ToString() + "'";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        //Deleting User
+        public void DeleteUser(int IDUser)
+        {
+            string query = "DELETE FROM User WHERE IDUser='" + IDUser + "'";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+
+        }
+        
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="FunctionName"></param>
+        /// <param name="FunctionCode"></param>
+        /// 
+        //Adding Function
+        public void AddFunction(string FunctionName, string FunctionCode)
+        {
+            string query = "INSERT INTO Function (FunctionName, FunctionCode) VALUES ('" +
+                FunctionName + "','" +
+                FunctionCode + "')";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+
+        }
+
+        //Updating  Function
+        public void UpdateFunction(int IDFunction, string FunctionName, string FunctionCode)
+        {
+            string query = "UPDATE Function SET FunctionName ='" + FunctionName
+                + "',  FunctionCode='" + FunctionCode
+                + "' WHERE IDFunction='" + IDFunction.ToString() + "'";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        //Deleting Function
+        public void DeleteFunction(int IDFunction)
+        {
+            string query = "DELETE FROM Function WHERE IDUser='" + IDFunction + "'";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+
+        }
+        
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="IDUser"></param>
+        /// <param name="FileName"></param>
+        /// 
+        //Adding History
+        public void AddHistory(int IDUser,int FileName)
+        {
+            string query = "INSERT INTO History (IDUser, FileName) VALUES ('" +
+                IDUser.ToString() + "','" +
+                FileName + "')";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+
+        }
+
+        //Updating  History
+        public void UpdateHistory(int IDHistory, int IDUser, int FileName)
+        {
+            string query = "UPDATE History SET IDUser ='" + IDUser.ToString()
+                + "',  FileName='" + FileName
+                + "' WHERE IDHistory='" + IDHistory.ToString() + "'";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        //Deleting History
+        public void DeleteHistory(int IDHistory)
+        {
+            string query = "DELETE FROM History WHERE IDHistory='" + IDHistory + "'";
+
+            SQLiteCommand command = new SQLiteCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { }
+
+        }
     }
 }
